471aa4c39ca50f2d160cc9711ca0fe6ddc270192
pabergman <alexander@fastly.com>
2014-07-19 17:29:56 +0100

    adding tests for two unique values in one test

--- lib/sinatra.rb
+++ lib/sinatra.rb
@@ -1,38 +1,54 @@
 require 'sinatra'
 require 'json'
 
-fname = Array.new
-sname = {}
 dualname = {}
 
 get '/' do
   'Hello world!'
 end
 
+oneunique = Array.new
+
 post '/unique/oneunique' do
 	request_payload = JSON.parse(request.body.read)
 	if(request_payload['name'] == nil)
-
-	elsif(fname.include?(request_payload['name']))
+		status 200
+	elsif(oneunique.include?(request_payload['name']))
 		 status 409
 	else 
-		fname << request_payload['name']
+		oneunique << request_payload['name']
 		status 200
 	end
-	body fname.to_json
+	body oneunique.to_json
 end
 
+twounique = Array.new
 
+post '/unique/twounique' do
+	request_payload = JSON.parse(request.body.read)
+
+	if(request_payload['firstname'] == nil || request_payload['surname'] == nil)
+		status 200
+	elsif(twounique.include?(request_payload['firstname']) || twounique.include?(request_payload['surname']))
+		 status 409
+	else 
+		twounique << request_payload['firstname']
+		twounique << request_payload['surname']
+		status 200
+	end
+
+	body twounique.to_json
+end
 
 get '/unique/sname' do
 	body sname.to_json
 end
 
 get '/unique/dualname' do
-	set :fname, "moo"
-	  "foo is set to " + settings.fname
+	set :oneunique, "moo"
+	  "foo is set to " + settings.oneunique
 end
 
-post '/fname' do
-  "foo is set to " + settings.fname
+post '/oneunique' do
+  "foo is set to " + settings.oneunique
 end
