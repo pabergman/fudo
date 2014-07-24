65120f8b278f5be1230869a42b4ca9ca02cfa474
pabergman <alexander@fastly.com>
2014-07-24 18:08:01 +0100

    Cleaned up data fudger & added support for changing types

--- lib/data_fudger.rb
+++ lib/data_fudger.rb
@@ -79,7 +79,7 @@ class DataFudger
       when 'Array'
         snowflake_array(value,i, fudger_spec[key])
       else
-        if(fudger_spec[key]['donotmodify'] == false)
+        if(fudger_spec[key]['rules']['donotmodify'] == false)
           m[key] = snowflake_value(value)
         end
       end
@@ -94,7 +94,7 @@ class DataFudger
       when 'Array'
         snowflake_array(value)
       else
-        if(fudger_spec['donotmodify'] == false)
+        if(!fudger_spec['donotmodify'])
           snowflake_value(value)
         end
       end
@@ -102,10 +102,10 @@ class DataFudger
   end
 
   def snowflake_value(value,  i = @fudged_data.size)
-    if(value.class.to_s == 'String')
+    if(value.kind_of?(String))
       value << "__#{i}"
-    elsif(value.class.to_s == 'Fixnum')
-      value += 1
+    elsif(value.kind_of?(Fixnum))
+      value += i
     end
     value
   end
@@ -122,78 +122,122 @@ class DataFudger
     y = Marshal.load(Marshal.dump(@origin_request))
     m = y['request']['body']
     snowflake(m)
-    m.delete(key)
-    if(@fudger_spec[key]['rules']['required'])
-      add_to_fudged(y, "#{key} is a required field", 400)
-    else
-      add_to_fudged(y, "#{key} is not a required field",200)
+    delete(y, m, key, @fudger_spec[key]['rules'])
+
+    y = Marshal.load(Marshal.dump(@origin_request))
+    m = y['request']['body']
+    snowflake(m)
+    set_nil(y, m, key, @fudger_spec[key]['rules'])
+
+    if(@fudger_spec[key]['type'] == 'value')
+      y = Marshal.load(Marshal.dump(@origin_request))
+      m = y['request']['body']
+      snowflake(m)
+      change_type(y, m, key, @fudger_spec[key]['rules'])
     end
 
     if(@fudger_spec[key]['type'] == 'value')
       y = Marshal.load(Marshal.dump(@origin_request))
       m = y['request']['body']
       snowflake(m)
-      m[key] = @request_body[key]
-      if(@fudger_spec[key]['rules']['unique'])
-        add_to_fudged(y, "#{key} should be unique",  409)
-      else
-        add_to_fudged(y, "#{key} should not need to be a unique field", 200)
-      end
+      set_unique(y, m, @request_body, key, @fudger_spec[key]['rules'])
     end
+
   end
 
   def depth_1(key)
     y = Marshal.load(Marshal.dump(@origin_request))
     m = y['request']['body']
     snowflake(m)
-    if(m[@depth[0]].class.to_s == 'Hash')
-      m[@depth[0]].delete(key)
-    else
-      m[@depth[0]][key] = nil
-    end
+    delete(y, m[@depth[0]], key, @fudger_spec[@depth[0]]['properties'][key]['rules'])
 
-    if(@fudger_spec[@depth[0]]['properties'][key]['rules']['required'])
-      add_to_fudged(y, "#{key} is a required field", 400)
-    else
-      add_to_fudged(y, "#{key} is not a required field", 200)
+    y = Marshal.load(Marshal.dump(@origin_request))
+    m = y['request']['body']
+    snowflake(m)
+    set_nil(y, m[@depth[0]], key, @fudger_spec[@depth[0]]['properties'][key]['rules'])
+
+    if(@fudger_spec[@depth[0]]['properties'][key]['type'] == 'value')
+      y = Marshal.load(Marshal.dump(@origin_request))
+      m = y['request']['body']
+      snowflake(m)
+      change_type(y, m[@depth[0]], key, @fudger_spec[@depth[0]]['properties'][key]['rules'])
     end
 
     if(@fudger_spec[@depth[0]]['properties'][key]['type'] == 'value')
       y = Marshal.load(Marshal.dump(@origin_request))
       m = y['request']['body']
       snowflake(m)
-      m[@depth[0]][key] = @request_body[@depth[0]][key]
-      if(@fudger_spec[@depth[0]]['properties'][key]['rules']['unique'])
-        add_to_fudged(y, "#{key} should be unique",  409)
-      else
-        add_to_fudged(y, "#{key} should not need to be a unique field", 200)
-      end
+      set_unique(y, m[@depth[0]], @request_body[@depth[0]], key, @fudger_spec[@depth[0]]['properties'][key]['rules'])
     end
+
   end
 
   def depth_2(key)
+
     y = Marshal.load(Marshal.dump(@origin_request))
     m = y['request']['body']
     snowflake(m)
-    m[@depth[0]][@depth[1]].delete(key)
-    if(@fudger_spec[@depth[0]]['properties'][@depth[1]]['properties'][key]['rules']['required'])
-      add_to_fudged(y, "#{key} is a required field", 400)
-    else
-      add_to_fudged(y, "#{key} is not a required field", 200)
+    delete(y, m[@depth[0]][@depth[1]], key, @fudger_spec[@depth[0]]['properties'][@depth[1]]['properties'][key]['rules'])
+
+    y = Marshal.load(Marshal.dump(@origin_request))
+    m = y['request']['body']
+    snowflake(m)
+    set_nil(y, m[@depth[0]][@depth[1]], key, @fudger_spec[@depth[0]]['properties'][@depth[1]]['properties'][key]['rules'])
+
+    if(@fudger_spec[@depth[0]]['properties'][@depth[1]]['properties'][key]['type'] == 'value')
+      y = Marshal.load(Marshal.dump(@origin_request))
+      m = y['request']['body']
+      snowflake(m)
+      change_type(y, m[@depth[0]][@depth[1]], key, @fudger_spec[@depth[0]]['properties'][@depth[1]]['properties'][key]['rules'])
     end
 
     if(@fudger_spec[@depth[0]]['properties'][@depth[1]]['properties'][key]['type'] == 'value')
       y = Marshal.load(Marshal.dump(@origin_request))
       m = y['request']['body']
       snowflake(m)
-      m[@depth[0]][@depth[1]][key] = @request_body[@depth[0]][@depth[1]][key]
-      if(@fudger_spec[@depth[0]]['properties'][@depth[1]]['properties'][key]['rules']['unique'])
-        add_to_fudged(y, "#{key} should be unique",  409)
-      else
-        add_to_fudged(y, "#{key} should not need to be a unique field", 200)
-      end
+      set_unique(y, m[@depth[0]][@depth[1]], @request_body[@depth[0]][@depth[1]], key, @fudger_spec[@depth[0]]['properties'][@depth[1]]['properties'][key]['rules'])
     end
 
+  end
+
+  def delete(y, m, key, rules)
+    m.delete(key)
+    if(rules['required'])
+      add_to_fudged(y, "#{key} is a required field", 400)
+    else
+      add_to_fudged(y, "#{key} is not a required field",200)
+    end
+  end
+
+  def set_nil(y, m, key, rules)
+    m[key] = nil
+    if(rules['required'])
+      add_to_fudged(y, "#{key} is a required field (nil error)", 400)
+    else
+      add_to_fudged(y, "#{key} is not a required field (nil error)",200)
+    end
+  end
+
+  def change_type(y, m, key, rules)
+    if(rules['value-type'] == "String")
+      m[key] = rand(10000)
+      add_to_fudged(y, "#{key} is a should be a string!", 400)
+    elsif(rules['value-type'] == "Fixnum")
+      m[key] = m[key].to_s
+      add_to_fudged(y, "#{key} is a should be a integer!", 400)
+    elsif(rules['value-type'] == "boolean")
+      m[key] = "true"
+      add_to_fudged(y, "#{key} is a should be a boolean!", 400)
+    end
+  end
+
+  def set_unique(y, m, original, key, rules)
+    m[key] = original[key]
+    if(rules['unique'])
+      add_to_fudged(y, "#{key} should be unique",  409)
+    else
+      add_to_fudged(y, "#{key} should not need to be a unique field", 200)
+    end
   end
 
 end
