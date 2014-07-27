97e8f37489cb7a85ff497fcc5618f6db0c19c7e1
pabergman <alexander@fastly.com>
2014-07-27 19:34:26 +0100

    Added initial support for json-schema validation

--- lib/run_test.rb
+++ lib/run_test.rb
@@ -9,12 +9,22 @@ require_relative 'data_fudger'
 class RunTest
 
   def self.run(full_request)
-    if(full_request['request']['method'] == "GET" || full_request['request']['method'] == "GET")
+
+    body_def = Marshal.load(Marshal.dump(full_request['request']['body']))
+    DataGenerator.generate_data(full_request['request']['body'])
+    full_request['request']['bodysrc'] = body_def
+
+    if(full_request['request']['method'] == "GET" || full_request['request']['method'] == "DELETE")
       request = no_body(full_request)
+
     else
+
+      data_fudger = DataFudger.new(full_request)
+      data_fudger.run
+
       with_body(full_request)
-      fudged_data = create_unhappy(full_request)
-      run_unhappy(fudged_data)
+      run_unhappy(data_fudger.fudged_data)
+
     end
 
   end
@@ -28,6 +38,8 @@ class RunTest
 
     request.run
 
+    puts JSON::Validator.validate(full_request['response']['body'], request.response.body)
+
   end
 
   def self.with_body(full_request)
@@ -40,18 +52,8 @@ class RunTest
 
     request.run
 
-  end
+    puts JSON::Validator.validate(full_request['response']['body'], request.response.body)
 
-  def self.create_unhappy(full_request)
-
-    body_def = Marshal.load(Marshal.dump(full_request['request']['body']))
-    DataGenerator.generate_data(full_request['request']['body'])
-    full_request['request']['bodysrc'] = body_def
-
-    data_fudger = DataFudger.new(full_request)
-    data_fudger.run
-
-    data_fudger.fudged_data
   end
 
   def self.run_unhappy(fudged_data)
@@ -65,34 +67,49 @@ class RunTest
         method: value['request']['method'],
         body: value['request']['body'].to_json,
         verbose: false,
-        headers: { 'Content-Type' => "application/json"}
+        headers: value['request']['headers']
       )
       bad_request.on_complete do | response |
         if (response.code != value['response']['status'])
           if(value['response']['status'].between?(400, 499) && response.code.between?(400,499))
-            value['response']['severity'] -= 1
+            value['response']['severity'] += 1
           elsif(value['response']['status'].between?(200, 299) && response.code.between?(200,299))
-            value['response']['severity'] -= 1
+            value['response']['severity'] += 1
           end
           unhappy_outout << {"severity" => value['response']['severity'], "expected" => value['response']['status'],
                              "received" => response.code, "message" => value['response']['message'], "request" => value['request']}
-        else
+        elsif(Fudo::CONFIG['print_success'] == true)
           unhappy_outout << {"severity" => 100, "expected" => value['response']['status'],
                              "received" => response.code, "message" => value['response']['message'], "request" => value['request']}
+
+        end
+
+        if(response.code == 200)
+          errors = JSON::Validator.fully_validate(value['response']['body'], response.body)
+          # puts value['response']['message']
+          puts errors
+        elsif(response.code != 500 && Fudo::CONFIG['error_body'] == true)
+
+          puts JSON::Validator.validate(value['response']['error_body'], response.body)
+
         end
       end
 
       hydra.queue bad_request
 
     end
-
     hydra.run
+
+    puts unhappy_outout.to_json
 
     puts "Severity | Expected | Received | Message"
 
     unhappy_outout.each do | value |
 
-      puts "#{value['severity']}        | #{value['expected']}      | #{value['received']}        | #{value['message']}"
+      print value['severity'].to_s.ljust(9)
+      print "| #{value['expected']}".ljust(11)
+      print "| #{value['received']}".ljust(11)
+      puts "| #{value['message']}"
 
     end
   end
