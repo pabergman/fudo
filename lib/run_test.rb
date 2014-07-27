c058461e523db568600932d569f2d29fa33d48a5
pabergman <alexander@fastly.com>
2014-07-27 20:57:59 +0100

    Cleaned up logging a bit further

--- lib/run_test.rb
+++ lib/run_test.rb
@@ -10,22 +10,43 @@ class RunTest
 
   def self.run(full_request)
 
+    return_array = Array.new
+
     body_def = Marshal.load(Marshal.dump(full_request['request']['body']))
     DataGenerator.generate_data(full_request['request']['body'])
     full_request['request']['bodysrc'] = body_def
 
     if(full_request['request']['method'] == "GET" || full_request['request']['method'] == "DELETE")
       request = no_body(full_request)
-
     else
 
       data_fudger = DataFudger.new(full_request)
       data_fudger.run
 
-      with_body(full_request)
-      run_unhappy(data_fudger.fudged_data)
+      response = with_body(full_request)
+      message = logging(response, full_request)
+      return_array << message
+      if(Fudo::CONFIG['force_unhappy'] == false && message['severity'] > 100)
+        return return_array
+      end
 
+      return_array.concat(run_unhappy(data_fudger.fudged_data))
     end
+
+    puts return_array.to_json
+
+    puts "Severity | Expected | Received | Message"
+
+    return_array.each do | value |
+      if(value['severity'] < 100 || Fudo::CONFIG['log_success'] == true)
+        print value['severity'].to_s.ljust(9)
+        print "| #{value['expected']}".ljust(11)
+        print "| #{value['received']}".ljust(11)
+        puts "| #{value['message']}"
+      end
+    end
+
+    return_array
 
   end
 
@@ -47,9 +68,9 @@ class RunTest
       body: full_request['request']['body'].to_json,
       headers: full_request['request']['headers']
     )
-
     request.run
 
+    request.response
   end
 
   def self.run_unhappy(fudged_data)
@@ -68,9 +89,7 @@ class RunTest
       bad_request.on_complete do | response |
 
         error_message = logging(response,value)
-        if(error_message['severity'] < 100 || Fudo::CONFIG['log_success'] == true)
-          unhappy_outout << error_message
-        end
+        unhappy_outout << error_message
       end
 
       hydra.queue bad_request
@@ -78,37 +97,25 @@ class RunTest
     end
     hydra.run
 
-    puts unhappy_outout.to_json
-
-    puts "Severity | Expected | Received | Message"
-
-    unhappy_outout.each do | value |
-
-      print value['severity'].to_s.ljust(9)
-      print "| #{value['expected']}".ljust(11)
-      print "| #{value['received']}".ljust(11)
-      puts "| #{value['message']}"
-
-    end
+    unhappy_outout
   end
 
   def self.logging(response, value)
-
-
-    error_message = {}
-
-    if (response.code != value['response']['status'])
+    if(response.code != value['response']['status'])
       if(value['response']['status'].between?(400, 499) && response.code.between?(400,499))
         value['response']['severity'] += 2
       elsif(value['response']['status'].between?(200, 299) && response.code.between?(200,299))
         value['response']['severity'] += 1
       end
-      error_message = {"severity" => value['response']['severity'], "expected" => value['response']['status'],
-                       "received" => response.code, "message" => value['response']['message'], "request" => value['request']}
+      error_message = {"severity" => value['response']['severity'] }
     else
-      error_message = {"severity" => 100, "expected" => value['response']['status'],
-                       "received" => response.code, "message" => value['response']['message'], "request" => value['request']}
+      error_message = {"severity" => 100}
     end
+    error_message['expected'] = value['response']['status']
+    error_message['received'] = response.code
+    error_message['message'] = value['response']['message']
+    error_message['request'] = value['request']
+
 
     if(response.code == 200)
       errors = JSON::Validator.fully_validate(value['response']['body'], response.body)
