d5a601d1fb4f85a287418c18bb87fa5cffb66801
pabergman <alexander@fastly.com>
2014-08-11 22:23:42 +0100

    Fixed a bunch of ruby style issues.

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,27 +2,23 @@
 require 'json'
 
 class FudoJsonGenerator
-
   def self.master(input, output)
-
     if input.is_a? Array
-      array(input,output)
+      array(input, output)
     elsif input.is_a? Hash
-      hash(input,output)
+      hash(input, output)
     else
-      value(input,output)
+      value(input, output)
     end
-
   end
 
   def self.array(input, output)
-
-    output['type'] = "array"
-    output['rules'] = Hash.new
+    output['type'] = 'array'
+    output['rules'] = {}
     output['rules']['min'] = 0
     output['rules']['max'] = input.length * 2
     output['rules']['repeat'] = false
-    output['properties'] = Hash.new
+    output['properties'] = {}
 
     input.each_with_index do |value, index|
 
@@ -31,18 +27,16 @@ class FudoJsonGenerator
       master(value, output['properties'][index])
 
     end
-
   end
 
   def self.hash(input, output)
+    output['type'] = 'object'
 
-    output['type'] = "object"
-
-    output['rules'] = Hash.new
+    output['rules'] = {}
 
     output['rules']['required'] = true
 
-    output['properties'] = Hash.new
+    output['properties'] = {}
     input.each do |key, value|
       output['properties'][key] = Hash.new
       master(value, output['properties'][key])
@@ -50,44 +44,36 @@ class FudoJsonGenerator
   end
 
   def self.value(input, output)
-    output['type'] = "value"
-    output['rules'] = Hash.new
+    output['type'] = 'value'
+    output['rules'] = {}
     output['rules']['value-type'] = input.class.to_s
     output['rules']['donotmodify'] = false
     output['rules']['required'] = true
     output['rules']['unique'] = false
 
-    output['properties'] = Hash.new
-    output['properties']['source'] = "fixed"
+    output['properties'] = {}
+    output['properties']['source'] = 'fixed'
     output['properties']['value'] = input
-    output['inputs'] = Array.new
+    output['inputs'] = []
   end
-
 end
 
-if __FILE__ == $0
-
-  puts "What method? (POST, PUT, GET, DELETE)"
+if __FILE__ == $PROGRAM_NAME
+  puts 'What method? (POST, PUT, GET, DELETE)'
   method = gets.chomp
-  puts "What URL?"
+  puts 'What URL?'
   url = gets.chomp
 
-  if(method == "POST" || method == "PUT")
-    puts "Request body JSON please."
+  if method == 'POST' || method == 'PUT'
+    puts 'Request body JSON please.'
     input = JSON.parse(gets.chomp)
-    request_body = JSON.parse("{}")
+    request_body = JSON.parse('{}')
     FudoJsonGenerator.master(input, request_body)
   else
-    request_body = JSON.parse("{}")
+    request_body = ''
   end
 
-
-  fudo_request = {"request" => {"method" => method, "url" => url, "headers" => {}, "body" => request_body},
-                  "response" => {"body" => "replace me with json-schema please!", "headers" => {}, "status" => 200, "message" => "should pass"}}
-
-  # shittystring = '[{"status": "new", "name": "alex", "id": 10},{"status": "new", "name": "alex", "id": 11}]'
-  # shittystring = '{"status": "new", "name": "alex", "id": {"status": "new", "name": [2,3,4,5]}}'
-  # shittystring = '{"": "new", "name": "alex", "id": {"status": "new"}}'
-
-
+  fudo_request = { 'request' => { 'method' => method, 'url' => url, 'headers' => {}, 'body' => request_body },
+                   'response' => { 'body' => 'replace me with json-schema please!', 'headers' => {}, 'status' => 200, 'message' => 'should pass' } }
+  puts JSON.pretty_generate(fudo_request)
 end
