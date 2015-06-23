9f3dddf41b5c59ef791cf0dc6af3c67cf78a03fd
pabergman <alexander@fastly.com>
2015-06-23 20:10:32 +0200

    Basic structures.

--- lib/fudo/schema_generator.rb
+++ lib/fudo/schema_generator.rb
@@ -1,11 +1,41 @@
 module Fudo
   class SchemaGenerator
-    def construct_schema(json_source)
+
+    attr_reader :output
+
+    def initialize(input)
+      @input = input
+      @output = {}
+      construct_schema(@input, @output)
+    end
+
+    def construct_schema(input, output)
+      case input
+      when Hash
+        object(input, output)
+      when Array
+        array(input, output)
+      else
+        value(input, output)
+      end
+
+      output['$schema'] = 'http://json-schema.org/draft-04/schema#'
+    end
+
+    def array(input, output)
+      output['type'] = 'array'
+    end
+
+    def object(input, output)
+      output['type'] = 'object'
+    end
+
+    def value(input, output)
 
     end
 
-    def self.schema_type(type)
-      case type
+    def self.schema_type(value)
+      case value
       when Hash
         return "object"
       when Array
--- test/fudo/schema_generator_test.rb
+++ test/fudo/schema_generator_test.rb
@@ -2,6 +2,18 @@ require_relative '../test_helper'
 
 class SchemaGeneratorTest < Minitest::Test
 
+  def test_empty_object
+    schema = Fudo::SchemaGenerator.new({}).output
+    assert_equal 'http://json-schema.org/draft-04/schema#', schema['$schema']
+    assert_equal 'object', schema['type']
+  end
+
+  def test_empty_array
+   schema = Fudo::SchemaGenerator.new([]).output
+   assert_equal 'http://json-schema.org/draft-04/schema#', schema['$schema']
+   assert_equal 'array', schema['type']
+  end
+
   def test_schema_type_identifier
     assert_equal 'object', Fudo::SchemaGenerator.schema_type({})
     assert_equal 'array', Fudo::SchemaGenerator.schema_type([])
